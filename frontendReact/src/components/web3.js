052386919647c1bf7d765d601cdbae2a1c18d325
habibkhan004 <hkhabib105@gmail.com>
2024-12-24 20:07:22 +0500

    web3 file added

--- frontendReact/src/components/web3.js
+++ frontendReact/src/components/web3.js
@@ -1,28 +1,14 @@
-// web3.js
-import Web3 from 'web3';
+import Web3 from "web3";
+import LifeCoin from "./build/contracts/LifeCoin.json";
 
-let web3;
-if (window.ethereum) {
-  web3 = new Web3(window.ethereum);
-  try {
-    window.ethereum.enable(); // Request permission to access accounts
-  } catch (error) {
-    console.error("User denied account access");
-  }
-} else if (window.web3) {
-  web3 = new Web3(window.web3.currentProvider);
-} else {
-  console.log("No Ethereum browser detected. You should consider trying MetaMask!");
-}
+const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+const networkId = await web3.eth.net.getId();
+const deployedNetwork = LifeCoin.networks[networkId];
+const instance = new web3.eth.Contract(
+  LifeCoin.abi,
+  deployedNetwork && deployedNetwork.address
+);
 
-const getContract = async () => {
-  const networkId = await web3.eth.net.getId();
-  const deployedNetwork = LifeCoin.networks[networkId];
-  const instance = new web3.eth.Contract(
-    LifeCoin.abi,
-    deployedNetwork && deployedNetwork.address,
-  );
-  return instance;
-};
-
-export default getContract;
+// Example: Check total supply
+const totalSupply = await instance.methods.totalSupply().call();
+console.log(totalSupply);

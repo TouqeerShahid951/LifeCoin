d9f0262f13ecb99af8ed3b12522b015c55a2e870
Touqeer_Shahid <touqeershahid951@gmail.com>
2024-12-25 01:17:38 +0500

    Testing complete

--- NodeJSBackend/test/LifeCoin.js
+++ NodeJSBackend/test/LifeCoin.js
@@ -1,20 +1,24 @@
 const LifeCoin = artifacts.require("LifeCoin");
 
 contract("LifeCoin", accounts => {
-  const [deployer, recipient, anotherAccount] = accounts;
+    it("should have correct total supply after deployment", async () => {
+        const instance = await LifeCoin.deployed();
+        const totalSupply = await instance.totalSupply();
 
-  it("should have correct total supply after deployment", async () => {
-    const instance = await LifeCoin.deployed();
-    const totalSupply = await instance.totalSupply();
-    assert.equal(totalSupply.toString(), "1000000", "Total supply should be 1,000,000");
-  });
+        // Adjust total supply to account for decimals
+        const expectedSupply = web3.utils.toWei('1000000', 'ether');
+        assert.equal(totalSupply.toString(), expectedSupply, "Total supply should be 1,000,000");
+    });
 
-  it("should transfer tokens correctly", async () => {
-    const instance = await LifeCoin.deployed();
-    await instance.transfer(recipient, 1000, { from: deployer });
-    const balance = await instance.balanceOf(recipient);
-    assert.equal(balance.toString(), "1000", "Recipient should have 1,000 LifeCoins");
-  });
+    it("should transfer tokens correctly", async () => {
+        const instance = await LifeCoin.deployed();
+        const recipient = accounts[1];
+        const amount = web3.utils.toWei('1000', 'ether');
 
-  // Add more test cases as needed
+        // Perform transfer
+        await instance.transfer(recipient, amount, { from: accounts[0] });
+
+        const balance = await instance.balanceOf(recipient);
+        assert.equal(balance.toString(), amount, "Recipient should have 1000 LifeCoins");
+    });
 });
